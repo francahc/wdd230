a4e2e1008bc768d665d678aff6973c7a85fd43fb
francahc <132031773+francahc@users.noreply.github.com>
2025-03-20 16:07:48 -0600

    .

--- scripts/mode.js
+++ scripts/mode.js
@@ -1,8 +1,31 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
 const main = document.querySelector("main");
 
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    body.classList.add('dark-mode');
+    header.classList.add('dark-mode');
+    footer.classList.add('dark-mode');
+    main.classList.add('dark-mode');
+    modeButton.textContent = '🕶️'; 
+} else {
+    modeButton.textContent = '💡'; 
+}
+
 modeButton.addEventListener("click", () => {
-	body.classList.toggle('dark-mode');
-	main.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    header.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+    main.classList.toggle('dark-mode');
+
+    
+    if (body.classList.contains('dark-mode')) {
+        modeButton.textContent = '🕶️';
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        modeButton.textContent = '💡';
+        localStorage.setItem('dark-mode', 'disabled');
+    }
 });
