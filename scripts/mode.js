c3456fb3c965e089b4a8ee04d3621fe72313531c
francahc <132031773+francahc@users.noreply.github.com>
2025-03-19 20:36:31 -0600

    .

--- scripts/mode.js
+++ scripts/mode.js
@@ -1,8 +1,14 @@
 const modeButton = document.querySelector("#mode");
-const body = document.querySelector("body");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-	body.classList.toggle('dark-mode');
-	main.classList.toggle('dark-mode');
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
 });
