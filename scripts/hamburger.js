60f6db4a1f9e8dc8ec4f89eeb00394c733515dd5
francahc <132031773+francahc@users.noreply.github.com>
2025-03-21 17:37:37 -0600

    .

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,7 +3,7 @@ const menu = document.querySelector("nav");
 
 if (hamburger) {
     hamburger.addEventListener("click", () => {
-        // Toggle the menu visibility and change the hamburger to 'X'
+       
         menu.classList.toggle("open");
         hamburger.classList.toggle("open");
     });
