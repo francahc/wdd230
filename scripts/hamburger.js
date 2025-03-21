5df3b94489f644e64422c97650fd7d73f2e1af85
francahc <132031773+francahc@users.noreply.github.com>
2025-03-20 20:08:35 -0600

    .

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -6,12 +6,5 @@ if (hamburger) {
         // Toggle the menu visibility and change the hamburger to 'X'
         menu.classList.toggle("open");
         hamburger.classList.toggle("open");
-        
-        // Change the hamburger icon to 'X' when the menu is open
-        if (hamburger.classList.contains("open")) {
-            hamburger.innerHTML = 'X'; // Change to 'X' when open
-        } else {
-            hamburger.innerHTML = '&#9776;'; // Change back to hamburger when closed
-        }
     });
 }
